7d1eb7ece5af9ade54e35a2046a92ec72a9f1b01
remi-boivin <remi.boivin@epitech.eu>
2023-04-06 15:29:23 +0200

    :white_check_mark: feat(decoder): Implement decoder dataclass
    we created a dataclass to decode gameboy instructions in order to print them

--- sample/disassembler/decoder.py
+++ sample/disassembler/decoder.py
@@ -30,6 +30,13 @@ class Decoder:
 
         Returns: A new `Decoder` instance.
         """
+        prefixed, regular = load_opcodes(opcode_file)
+        return cls(
+            prefixed_instructions=prefixed,
+            instructions=regular,
+            data=data,
+            address=address,
+        )
 
     def read(self, address: int, count: int = 1):
         """
@@ -42,6 +49,11 @@ class Decoder:
 
         Raises: IndexError: If the address is out of range.
         """
+        if 0 <= address + count <= len(self.data):
+            v = self.data[address: address + count]
+            return int.from_bytes(v, sys.byteorder)
+        else:
+            raise IndexError(f'{address=}+{count=} is out of range')
 
     def decode(self, address: int):
         """
@@ -53,3 +65,28 @@ class Decoder:
 
         Raises: IndexError: If the address is out of range.
         """
+        opcode = None
+        decoded_instruction = None
+        opcode = self.read(address)
+        if opcode == 0x00:
+            raise IndexError(f'{address=} is out of range')
+        address += 1
+        # 0xCB is a special prefix instruction. Read from
+        # prefixed_instructions instead and increment address.
+        if opcode == 0xCB:
+            opcode = self.read(address)
+            address += 1
+            instruction = self.prefixed_instructions[opcode]
+        else:
+            instruction = self.instructions[opcode]
+        new_operands = []
+        for operand in instruction.operands:
+            if operand.bytes is not None:
+                value = self.read(address, operand.bytes)
+                address += operand.bytes
+                new_operands.append(operand.copy(value))
+            else:
+                # No bytes; that means it's not a memory address
+                new_operands.append(operand)
+        decoded_instruction = instruction.copy(operands=new_operands)
+        return address, decoded_instruction
